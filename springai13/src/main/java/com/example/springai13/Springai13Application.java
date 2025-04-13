0e42651be9c5e0d3297423abbbe1d863da896e52
김가남 <devkgn88@gmail.com>
2025-04-13 14:10:53 +0900

    [김가남] docker-compose 사용해서 vector 구조 셋팅하기

---
+++ springai13/src/main/java/com/example/springai13/Springai13Application.java
@@ -0,0 +1,12 @@
+package com.example.springai13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springai13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springai13Application.class, args);
+	}
+}
