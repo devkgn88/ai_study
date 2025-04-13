04912329c4e1b782e3a60bd7f9962d5c101a772e
김가남 <devkgn88@gmail.com>
2025-04-13 10:11:32 +0900

    [김가남] text to audio, audio to text, txt -><- img

---
+++ springai7/src/main/java/com/example/springai7/Springai7Application.java
@@ -0,0 +1,13 @@
+package com.example.springai7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springai7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springai7Application.class, args);
+	}
+
+}
